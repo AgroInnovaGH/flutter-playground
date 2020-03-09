68b53dc05b180ba2710e244b4d579977d40de86f
Daniel Sirakov <dsirakoff@gmail.com>
2020-03-09 15:51:50 +0000

    Auth So far

---
+++ todo/lib/SignIn.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:international_phone_input/international_phone_input.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  String phoneNumber;
+  String phoneIsoCode;
+  bool visible = false;
+  String confirmedNumber = '';
+
+  void onPhoneNumberChange(
+      String number, String internationalizedPhoneNumber, String isoCode) {
+    print(number);
+    setState(() {
+      phoneNumber = number;
+      phoneIsoCode = isoCode;
+    });
+  }
+
+  onValidPhoneNumber(
+      String number, String internationalizedPhoneNumber, String isoCode) {
+    setState(() {
+      visible = true;
+      confirmedNumber = internationalizedPhoneNumber;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 0.0, bottom: 0.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+            InternationalPhoneInput(
+              onPhoneNumberChange: onPhoneNumberChange,
+              initialPhoneNumber: phoneNumber,
+              initialSelection: phoneIsoCode,
+              enabledCountries: ['+233', '+1'],
+              labelText: "Phone Number",
+            ),
+            SizedBox(height: 30),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Sign in / Sign up')
+            )
+        ],
+      ),
+    );
+  }
+}
--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'FloatButton.dart';
 import 'Task.dart';
 import 'ListWidget.dart';
+// import 'Wrapper.dart';
+import 'SignIn.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
@@ -101,64 +103,64 @@ class _HomeState extends State<Home>{
         title: Text('TODO App'),
         backgroundColor: Colors.green[600],
       ),
-      body: tabs[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.green[600],
-        currentIndex: currentIndex,
-        onTap: (index){
+      body: SignIn(),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   backgroundColor: Colors.green[600],
+      //   currentIndex: currentIndex,
+      //   onTap: (index){
 
-          setState(() {
+      //     setState(() {
             
-            currentIndex = index;
+      //       currentIndex = index;
 
-          });
+      //     });
 
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list, color: Colors.white),
-            activeIcon: Icon(Icons.list, color: Colors.green[800]),
-            title: Text('All', style: TextStyle(
-              color: Colors.white
-            ))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted, color: Colors.white),
-            activeIcon: Icon(Icons.format_list_bulleted, color: Colors.green[800]),
-            title: Text('Not Completed', style: TextStyle(
-              color: Colors.white
-            ))
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.playlist_add_check, color: Colors.white),
-            activeIcon: Icon(Icons.playlist_add_check, color: Colors.green[800]),
-            title: Text('Completed', style: TextStyle(
-              color: Colors.white
-            ))
-          )
-        ],
-      ),
-      floatingActionButton: FloatButton(
-        onCallback: (String text) async {
+      //   },
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.list, color: Colors.white),
+      //       activeIcon: Icon(Icons.list, color: Colors.green[800]),
+      //       title: Text('All', style: TextStyle(
+      //         color: Colors.white
+      //       ))
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.format_list_bulleted, color: Colors.white),
+      //       activeIcon: Icon(Icons.format_list_bulleted, color: Colors.green[800]),
+      //       title: Text('Not Completed', style: TextStyle(
+      //         color: Colors.white
+      //       ))
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.playlist_add_check, color: Colors.white),
+      //       activeIcon: Icon(Icons.playlist_add_check, color: Colors.green[800]),
+      //       title: Text('Completed', style: TextStyle(
+      //         color: Colors.white
+      //       ))
+      //     )
+      //   ],
+      // ),
+      // floatingActionButton: FloatButton(
+      //   onCallback: (String text) async {
 
-          if (tasks.indexWhere((task) => task.text == text) > -1) {
+      //     if (tasks.indexWhere((task) => task.text == text) > -1) {
 
-            print('Task already exists');
+      //       print('Task already exists');
 
-          } else {
+      //     } else {
 
-            await Firestore.instance.collection('tasks').add(<String, dynamic>{
-              'text': text,
-              'completed': false,
-              'created_at': FieldValue.serverTimestamp(),
-            });
+      //       await Firestore.instance.collection('tasks').add(<String, dynamic>{
+      //         'text': text,
+      //         'completed': false,
+      //         'created_at': FieldValue.serverTimestamp(),
+      //       });
 
-          }
+      //     }
 
-          print(tasks.length);
+      //     print(tasks.length);
 
-        }
-      ),
+      //   }
+      // ),
     );
 
   }
